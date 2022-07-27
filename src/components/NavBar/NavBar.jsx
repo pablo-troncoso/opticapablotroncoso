01338be0bedc5517c335af80515651c77254c601
pablotroncoso <pablotroncoso@gmail.com>
2022-07-27 02:18:40 -0400

    Componentes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,14 +3,19 @@
 // importarlo en App
 import "./navbar.css"
 export default function NavBar() {
+
+    function handleClick(){
+     console.log("click");
+    }    
+
     return (
         <nav className="nav-main">
-            <ul>
-                <li>Modelos</li>
-                <li>Cristales</li>
-                <li>Filtros</li>
-                <li>Lentes</li>
-                <li>Contacto</li>
+            <ul className = "nav-list">
+                <li onClick={handleClick}>Modelos</li>
+                <li onClick={handleClick}>Cristales</li>
+                <li onClick={handleClick}>Filtros</li>
+                <li onClick={handleClick}>Lentes</li>
+                <li onClick={handleClick}>Contacto</li>
             </ul>
         </nav>
     );
