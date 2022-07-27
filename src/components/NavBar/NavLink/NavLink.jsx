86594abc8bc216b5481a607a1bfeeddf3cf12f3f
pablotroncoso <pablotroncoso@gmail.com>
2022-07-27 02:31:25 -0400

    Nav actualizando

--- src/components/NavBar/NavLink/NavLink.jsx
+++ src/components/NavBar/NavLink/NavLink.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
 export default function NavLink() {
   return (
     <div>NavLink</div>
   )
 }
+
+return (
+    <nav className="nav-main">
+        <ul className = "nav-list">
+            <li onClick={handleClick}>Modelos</li>
+            <li onClick={handleClick}>Cristales</li>
+            <li onClick={handleClick}>Filtros</li>
+            <li onClick={handleClick}>Lentes</li>
+            <li onClick={handleClick}>Contacto</li>
+        </ul>
+    </nav>
+);
